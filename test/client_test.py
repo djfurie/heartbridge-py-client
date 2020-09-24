8b4d44ed0ea4a75b8a928ea4032173d3db664b3b
Dan Furie <dan.furie@gmail.com>
2020-09-23 22:06:59 -0400

    More tests

--- test/client_test.py
+++ test/client_test.py
@@ -7,6 +7,7 @@ import jwt
 import json
 import random
 import concurrent.futures
+import datetime
 
 # Some Mock performance information to be used for token generation and checking
 MOCK_ARTIST = "DJFury"
@@ -71,7 +72,7 @@ def token(client):
 
     logging.debug("Returned payload: %s", ret_val)
     p = json.loads(ret_val)
-    yield client, p
+    yield p
 
 
 def test_connect(client):
@@ -82,19 +83,44 @@ def test_connect(client):
     logging.info("Connection closed")
 
 
+def test_invalid_action(client):
+    client._ws.send(json.dumps({'action': 'bad-action'}))
+    ret = client.wait_for_data()
+    logging.debug(ret)
+
+    assert "error" in json.loads(ret)
+
+
+@pytest.mark.parametrize("bad_date", [datetime.datetime.now() - datetime.timedelta(minutes=6),
+                                      datetime.datetime.now() + datetime.timedelta(days=367)])
+def test_register_bad_date(client, bad_date):
+    ret = client.register(artist=MOCK_ARTIST,
+                          title=MOCK_TITLE,
+                          performance_date=bad_date.timestamp())
+    logging.debug(ret)
+    assert "error" in json.dumps(ret)
+
+
+def test_register_bad_artist(client):
+    ret = client.register(artist="A" * 65,
+                          title="B" * 65,
+                          performance_date=datetime.datetime.now().timestamp())
+    logging.debug(ret)
+    assert "error" in json.dumps(ret)
+
+
 def test_register(token):
     """ Register a performance - make sure that a token is returned """
-    client, p = token
 
     # Extract the Performance Id that was provided
-    performance_id = p['performance_id']
+    performance_id = token['performance_id']
     logging.info("PerformanceID: %s", performance_id)
 
     # Performance Ids are expected to be 6 characters long
     assert len(performance_id) == 6
 
     # Decode the token
-    token_claims = jwt.decode(p['token'], verify=False)
+    token_claims = jwt.decode(token['token'], verify=False)
     logging.debug(token_claims)
 
     assert token_claims['artist'] == MOCK_ARTIST
@@ -102,19 +128,22 @@ def test_register(token):
 
 
 @pytest.mark.parametrize("field,new_value", [('artist', 'DJFurioso'), ('title', 'A Fresh Title')])
-def test_update(token, field, new_value):
+def test_update(client, token, field, new_value):
     """ Register a performance, then update information """
-    client, p = token
-
-    performance_id = p['performance_id']
+    performance_id = token['performance_id']
     logging.info("PerformanceID: %s", performance_id)
 
-    orig_token_claims = jwt.decode(p['token'], verify=False)
+    orig_token_claims = jwt.decode(token['token'], verify=False)
     logging.debug(orig_token_claims)
 
     # Update the field information
-    ret_val = client.update(p['token'], {field: new_value})
+    ret_val = client.update(token['token'], {field: new_value})
     p = json.loads(ret_val)
+
+    # Make sure an error wasn't returned
+    if "error" in p:
+        logging.debug(p)
+        assert False
 
     # Check to make sure the performance id has remained the same
     assert p['performance_id'] == performance_id
@@ -123,6 +152,24 @@ def test_update(token, field, new_value):
     new_token_claims = jwt.decode(p['token'], verify=False)
     logging.debug(new_token_claims)
     assert new_token_claims[field] == new_value
+
+
+@pytest.mark.parametrize("field,new_value",
+                         [('artist', 'DJFurioso' * 65), ('token', 'ABC123456890'), ('performance_date', 0)])
+def test_update_bad_values(client, token, field, new_value):
+    """ Register a performance, then update information with bad values """
+    performance_id = token['performance_id']
+    logging.info("PerformanceID: %s", performance_id)
+
+    orig_token_claims = jwt.decode(token['token'], verify=False)
+    logging.debug(orig_token_claims)
+
+    # Update the field information
+    ret_val = client.update(token['token'], {field: new_value})
+    p = json.loads(ret_val)
+
+    # Make sure an error was returned
+    assert "error" in p, "Oops, we expected to get an error returned here"
 
 
 def test_publish(publisher: Publisher):
@@ -149,6 +196,7 @@ def test_subscribe(publisher: Publisher, url, num_subscriptions):
             logging.debug("Client: %s", tclient.connection_id)
             try:
                 ret = tclient.wait_for_data()
+                logging.debug(ret)
                 p = json.loads(ret)
                 if 'heartrate' in p:
                     logging.debug("Client: %s -- Got heart rate update: %s", client.connection_id, ret)
@@ -166,6 +214,7 @@ def test_subscribe(publisher: Publisher, url, num_subscriptions):
     pool = concurrent.futures.ThreadPoolExecutor(max_workers=num_subscriptions)
     for i in range(num_subscriptions):
         # Create the client
+        logging.debug("Connecting to: %s", url)
         client = heartbridge.Client(url)
 
         # Subscribe to a performance
@@ -173,13 +222,6 @@ def test_subscribe(publisher: Publisher, url, num_subscriptions):
 
         # Submit the client run loop to the thread pool
         num_rx_future = pool.submit(client_loop, client, stop_event)
-
-        # In order for the thread pool to kick things off, we need to request the result
-        # However, we don't really care about the result right now, so timeout immediately and move on
-        try:
-            num_rx_future.result(timeout=0)
-        except concurrent.futures.TimeoutError:
-            pass
 
         # Store it
         clients.append((client, num_rx_future))
