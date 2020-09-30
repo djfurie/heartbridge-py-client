1bc59aab588161ed2bd0f9c723d1527a2fa18bfc
Dan Furie <dan.furie@gmail.com>
2020-09-29 20:38:19 -0400

    Change tests over to using websockets library with asyncio

--- heartbridge/Client.py
+++ heartbridge/Client.py
@@ -1,7 +1,7 @@
 import datetime
 import json
 import logging
-import websocket
+import websockets
 
 logger = logging.getLogger(__name__)
 
@@ -9,56 +9,62 @@ logger = logging.getLogger(__name__)
 class Client:
     def __init__(self, url):
         self._websocket_url = url
-        self._ws = websocket.create_connection(url, timeout=5, enable_multithread=True)
+        self._ws: websockets.WebSocketClientProtocol = None
 
     @property
     def is_connected(self):
-        return self._ws.connected
+        if not self._ws:
+            return False
+        return self._ws.state == websockets.protocol.State.OPEN
 
     @property
     def connection_id(self):
-        return self._ws.headers['sec-websocket-accept']
+        return self._ws.request_headers['Sec-WebSocket-Key']
 
-    def close(self):
-        self._ws.close()
+    async def connect(self, url=None):
+        if url:
+            self._websocket_url = url
+        self._ws = await websockets.connect(self._websocket_url)
 
-    def subscribe(self, performance_id):
+    async def close(self):
+        await self._ws.close()
+
+    async def subscribe(self, performance_id):
         logger.info("Subscribing to Performance ID: %s", performance_id)
-        self._ws.send(json.dumps({'action': 'subscribe',
-                                  'performance_id': performance_id}))
+        await self._ws.send(json.dumps({'action': 'subscribe',
+                                        'performance_id': performance_id}))
 
-    def register(self, artist: str, title: str, performance_date: int = int(datetime.datetime.now().timestamp())):
+    async def register(self, artist: str, title: str, performance_date: int = int(datetime.datetime.now().timestamp())):
         logger.info("Requesting token for time %d", performance_date)
 
-        self._ws.send(json.dumps({
+        await self._ws.send(json.dumps({
             'action': 'register',
             'artist': artist,
             'title': title,
             'performance_date': performance_date}))
 
-        return self._ws.recv()
+        return await self._ws.recv()
 
-    def update(self, token, updated_info):
+    async def update(self, token, updated_info):
         cmd_json = {
             'action': 'update',
             'token': token
         }
 
-        self._ws.send(json.dumps({**cmd_json, **updated_info}))
-        return self._ws.recv()
+        await self._ws.send(json.dumps({**cmd_json, **updated_info}))
+        return await self._ws.recv()
 
-    def publish(self, token, heartrate):
+    async def publish(self, token, heartrate):
         cmd_json = {
             'action': 'publish',
             'heartrate': heartrate,
             'token': token
         }
 
-        self._ws.send(json.dumps(cmd_json))
-        return self._ws.recv()
+        await self._ws.send(json.dumps(cmd_json))
 
-    def wait_for_data(self):
-        return self._ws.recv()
+    async def wait_for_data(self):
+        return await self._ws.recv()
 
     def peek_rx(self):
         return len(self._ws.frame_buffer.recv_buffer)
--- test/client_test.py
+++ test/client_test.py
@@ -2,12 +2,12 @@ import pytest
 import logging
 import heartbridge
 import time
-import threading
 import jwt
 import json
 import random
 import concurrent.futures
 import datetime
+import asyncio
 
 # Some Mock performance information to be used for token generation and checking
 MOCK_ARTIST = "DJFury"
@@ -15,96 +15,102 @@ MOCK_TITLE = "Dougie's Furry Beats"
 
 
 class Publisher:
-    def __init__(self, token):
-        client, p = token
+    def __init__(self, client, token):
         self.client = client
-        self.token = p['token']
-        self.performance_id = p['performance_id']
-        self._publish_thread = threading.Thread(target=self._thread_func)
+        self.token = token['token']
+        self.performance_id = token['performance_id']
         self._interval = 1.0
-        self._running = True
-        self._event = threading.Event()
+        self._running = False
         self.total_events_published = 0
+        self._run_task = None
 
-    def _thread_func(self):
+    async def _thread_func(self):
+        logging.debug("Publisher thread running")
         while self._running:
             logging.debug("Publisher thread publishing")
             heart_rate = random.randint(60, 180)
-            ret_val = self.client.publish(self.token, heart_rate)
+            await self.client.publish(self.token, heart_rate)
             self.total_events_published += 1
-            hr = json.loads(ret_val)
-            assert hr['heartrate'] == heart_rate
-            self._event.wait(timeout=self._interval)
+            await asyncio.sleep(self._interval)
 
-    def start(self, interval: float = 1.0):
+    async def start(self, interval: float = 1.0):
         logging.debug("Publisher thread start")
         self._interval = interval
-        self._publish_thread.start()
+        self._running = True
+        self._run_task = asyncio.create_task(self._thread_func())
 
-    def stop(self):
+    async def stop(self):
         logging.debug("Publisher thread stop")
         self._running = False
-        self._event.set()
-        self._publish_thread.join(timeout=1.0)
+        self._run_task.cancel()
+        try:
+            await self._run_task
+        except asyncio.exceptions.CancelledError:
+            pass
 
 
 @pytest.fixture()
-def publisher(token):
-    yield Publisher(token)
+def publisher(client, token):
+    yield Publisher(client, token)
 
 
 @pytest.fixture()
-def client(url):
+async def client(url):
     logging.debug("Connecting to %s", url)
     c = heartbridge.Client(url)
+    await c.connect()
     yield c
     logging.debug("Closing connection")
-    c.close()
+    await c.close()
 
 
 @pytest.fixture()
-def token(client):
+async def token(client):
     logging.debug("Requesting new token")
 
     # Get a token for right now
-    ret_val = client.register(artist=MOCK_ARTIST,
-                              title=MOCK_TITLE)
+    ret_val = await client.register(artist=MOCK_ARTIST,
+                                    title=MOCK_TITLE)
 
     logging.debug("Returned payload: %s", ret_val)
     p = json.loads(ret_val)
     yield p
 
 
-def test_connect(client):
+@pytest.mark.asyncio
+async def test_connect(client):
     assert client.is_connected
     logging.info("Connection established... closing connection")
-    client.close()
+    await client.close()
     assert not client.is_connected
     logging.info("Connection closed")
 
 
-def test_invalid_action(client):
-    client._ws.send(json.dumps({'action': 'bad-action'}))
-    ret = client.wait_for_data()
+@pytest.mark.asyncio
+async def test_invalid_action(client):
+    await client._ws.send(json.dumps({'action': 'bad-action'}))
+    ret = await client.wait_for_data()
     logging.debug(ret)
 
     assert "error" in json.loads(ret)
 
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize("bad_date", [datetime.datetime.now() - datetime.timedelta(minutes=6),
                                       datetime.datetime.now() + datetime.timedelta(days=367)])
-def test_register_bad_date(client, bad_date):
-    ret = client.register(artist=MOCK_ARTIST,
-                          title=MOCK_TITLE,
-                          performance_date=bad_date.timestamp())
+async def test_register_bad_date(client, bad_date):
+    ret = await client.register(artist=MOCK_ARTIST,
+                                title=MOCK_TITLE,
+                                performance_date=bad_date.timestamp())
     logging.debug(ret)
     assert "error" in json.dumps(ret)
 
 
-def test_register_bad_artist(client):
-    ret = client.register(artist="A" * 65,
-                          title="B" * 65,
-                          performance_date=datetime.datetime.now().timestamp())
+@pytest.mark.asyncio
+async def test_register_bad_artist(client):
+    ret = await client.register(artist="A" * 65,
+                                title="B" * 65,
+                                performance_date=datetime.datetime.now().timestamp())
     logging.debug(ret)
     assert "error" in json.dumps(ret)
 
@@ -127,8 +133,9 @@ def test_register(token):
     assert token_claims['title'] == MOCK_TITLE
 
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize("field,new_value", [('artist', 'DJFurioso'), ('title', 'A Fresh Title')])
-def test_update(client, token, field, new_value):
+async def test_update(client, token, field, new_value):
     """ Register a performance, then update information """
     performance_id = token['performance_id']
     logging.info("PerformanceID: %s", performance_id)
@@ -137,7 +144,7 @@ def test_update(client, token, field, new_value):
     logging.debug(orig_token_claims)
 
     # Update the field information
-    ret_val = client.update(token['token'], {field: new_value})
+    ret_val = await client.update(token['token'], {field: new_value})
     p = json.loads(ret_val)
 
     # Make sure an error wasn't returned
@@ -154,9 +161,10 @@ def test_update(client, token, field, new_value):
     assert new_token_claims[field] == new_value
 
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize("field,new_value",
                          [('artist', 'DJFurioso' * 65), ('token', 'ABC123456890'), ('performance_date', 0)])
-def test_update_bad_values(client, token, field, new_value):
+async def test_update_bad_values(client, token, field, new_value):
     """ Register a performance, then update information with bad values """
     performance_id = token['performance_id']
     logging.info("PerformanceID: %s", performance_id)
@@ -165,23 +173,25 @@ def test_update_bad_values(client, token, field, new_value):
     logging.debug(orig_token_claims)
 
     # Update the field information
-    ret_val = client.update(token['token'], {field: new_value})
+    ret_val = await client.update(token['token'], {field: new_value})
     p = json.loads(ret_val)
 
     # Make sure an error was returned
     assert "error" in p, "Oops, we expected to get an error returned here"
 
 
-def test_publish(publisher: Publisher):
+@pytest.mark.asyncio
+async def test_publish(publisher: Publisher):
     """ Test publishing heartrate """
 
-    publisher.start(interval=0.1)
-    time.sleep(5)
-    publisher.stop()
+    await publisher.start(interval=0.1)
+    await asyncio.sleep(5)
+    await publisher.stop()
 
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize('num_subscriptions', [1, 2, 10, 100])
-def test_subscribe(publisher: Publisher, url, num_subscriptions):
+async def test_subscribe(publisher: Publisher, url, num_subscriptions):
     """
     This test is pretty bad right now.  There are all sorts of timing things that may break it
     It's a generally decent way to test basic functions in the server, but much better testing
@@ -190,12 +200,12 @@ def test_subscribe(publisher: Publisher, url, num_subscriptions):
 
     # This is the main loop for subscribers.  This will need to run in a thread per subscriber
     # because there is no timeout mechanism for wait_for_data.
-    def client_loop(tclient: heartbridge.Client, tstop_event: threading.Event):
+    async def client_loop(tclient: heartbridge.Client):
         num_rx = 0
         while True:
             logging.debug("Client: %s", tclient.connection_id)
             try:
-                ret = tclient.wait_for_data()
+                ret = await tclient.wait_for_data()
                 logging.debug(ret)
                 p = json.loads(ret)
                 if 'heartrate' in p:
@@ -204,43 +214,43 @@ def test_subscribe(publisher: Publisher, url, num_subscriptions):
                 if 'active_subcriptions' in p:
                     logging.debug("Client: %s -- Active Subcriptions: %s", client.connection_id, ret)
             except Exception:
-                # Capture any expection and use that to return here.  The expectation is that we will
+                # Capture any exception and use that to return here.  The expectation is that we will
                 # get a socket closed exception when the main test body closes all of the connections down
                 return num_rx
 
     # Set up all of the subscribers...
     clients = []
-    stop_event = threading.Event()
-    pool = concurrent.futures.ThreadPoolExecutor(max_workers=num_subscriptions)
     for i in range(num_subscriptions):
         # Create the client
         logging.debug("Connecting to: %s", url)
         client = heartbridge.Client(url)
+        await client.connect()
 
         # Subscribe to a performance
-        client.subscribe(publisher.performance_id)
+        await client.subscribe(publisher.performance_id)
 
         # Submit the client run loop to the thread pool
-        num_rx_future = pool.submit(client_loop, client, stop_event)
+        t = asyncio.create_task(client_loop(client))
 
         # Store it
-        clients.append((client, num_rx_future))
+        clients.append((client, t))
 
     # Start the publisher
-    publisher.start(interval=1)
+    await publisher.start(interval=1)
 
     # After a certain number of events, have the publisher stop
     while True:
+        await asyncio.sleep(0.1)
         if publisher.total_events_published == 10:
             break
 
-    publisher.stop()
+    await publisher.stop()
 
     # Shutdown each subscriber connection (which will terminate their thread) and gather up the results
     total_num_rx = 0
-    for iclient, x in clients:
-        iclient.close()
-        result = x.result(timeout=10)
+    for iclient, t in clients:
+        await iclient.close()
+        result = await t
         total_num_rx += result
 
     assert total_num_rx == publisher.total_events_published * num_subscriptions
